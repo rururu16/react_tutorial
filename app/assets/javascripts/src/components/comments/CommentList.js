d62b465fb9c040885a61c74e9b39ef08be052cc7
rururu16 <ynk618172@gmail.com>
2018-11-08 11:30:32 +0900

    finish tutorial

---
+++ app/assets/javascripts/src/components/comments/CommentList.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Comment from './Comment'
+
+// JSON形式のデータモデル
+const data = [
+  {
+    id: 1,
+    author: 'Pete Hunt',
+    text: 'This is one comment',
+  },
+  {
+    id: 2,
+    author: 'Jordan Walke',
+    text: 'This is another comment',
+  },
+]
+export default class CommentList extends React.Component {
+  render() {
+    // dataの中身1件ずつを変数commentに入れて展開
+    const commentNodes = data.map((comment) => {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      )
+    })
+    return (
+      <div className='commentList'>
+        {commentNodes}
+      </div>
+    )
+
+  }
+}
